f6b6706a4071cf4dc02cca608e234ef3255b6009
Devendra Varma <devendrawarma@gmail.com>
2023-01-22 20:49:58 +0530

    fixed aspect ratio using projection matrix

--- app/src/main/java/com/airhockey/android/AirHockeyRenderer.kt
+++ app/src/main/java/com/airhockey/android/AirHockeyRenderer.kt
@@ -3,6 +3,7 @@ package com.airhockey.android
 import android.content.Context
 import android.opengl.GLES20.*
 import android.opengl.GLSurfaceView.Renderer
+import android.opengl.Matrix.orthoM
 import android.util.Log
 import com.airhockey.android.util.LoggerConfig
 import com.airhockey.android.util.ShaderHelper
@@ -12,6 +13,8 @@ import java.nio.ByteOrder
 import java.nio.FloatBuffer
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
+import kotlin.math.max
+import kotlin.math.min
 
 class AirHockeyRenderer(private val context: Context) : Renderer {
 
@@ -24,15 +27,16 @@ class AirHockeyRenderer(private val context: Context) : Renderer {
             (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT
         private const val A_POSITION = "a_Position"
         private const val A_COLOR = "a_Color"
+        private const val U_MATRIX = "u_Matrix"
 
         val tableVerticesWithTriangles = floatArrayOf(
             // Triangle 1
             +0.0f, +0.0f, 1.0f, 1.0f, 1.0f,
-            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-            +0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-            +0.5f, +0.5f, 0.7f, 0.7f, 0.7f,
-            -0.5f, +0.5f, 0.7f, 0.7f, 0.7f,
-            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.8f, 0.0f, 0.0f, 0.7f,
+            +0.5f, -0.8f, 0.0f, 0.0f, 0.7f,
+            +0.5f, +0.8f, 0.0f, 0.0f, 0.7f,
+            -0.5f, +0.8f, 0.0f, 0.0f, 0.7f,
+            -0.5f, -0.8f, 0.0f, 0.0f, 0.7f,
 
             // Line 1
             -0.5f, 0f, 1.0f, 0.0f, 0.0f,
@@ -44,9 +48,12 @@ class AirHockeyRenderer(private val context: Context) : Renderer {
         )
     }
 
+    private val projectionMatrix = FloatArray(16)
+
     private var program: Int = 0
     private var aPositionLocation = 0
     private var aColorLocation = 0
+    private var uMatrixLocation = 0
 
     private val vertexData: FloatBuffer =
         ByteBuffer.allocateDirect(tableVerticesWithTriangles.size * BYTES_PER_FLOAT)
@@ -72,6 +79,7 @@ class AirHockeyRenderer(private val context: Context) : Renderer {
         glUseProgram(program)
         aPositionLocation = glGetAttribLocation(program, A_POSITION)
         aColorLocation = glGetAttribLocation(program, A_COLOR)
+        uMatrixLocation = glGetUniformLocation(program, U_MATRIX)
 
 
         vertexData.position(0)
@@ -102,6 +110,16 @@ class AirHockeyRenderer(private val context: Context) : Renderer {
             Log.w(TAG, "onSurfaceChanged: size = ($width, $height)")
         }
         glViewport(0, 0, width, height)
+
+        val aspectRatio = max(width, height).toFloat() / min(width, height).toFloat()
+        if (width > height) {
+            // Landscape
+            orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f)
+        } else {
+            // Portrait
+            orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f)
+        }
+        glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0)
     }
 
     override fun onDrawFrame(p0: GL10?) {
