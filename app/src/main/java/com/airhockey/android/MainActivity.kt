185c2105e2bb75166cbc2ac1318e791361a83bf6
Devendra Varma <devendrawarma@gmail.com>
2023-01-29 22:47:39 +0530

    used viewBinding and updated libraries

--- app/src/main/java/com/airhockey/android/MainActivity.kt
+++ app/src/main/java/com/airhockey/android/MainActivity.kt
@@ -1,23 +1,40 @@
 package com.airhockey.android
 
 import android.app.ActivityManager
-import android.opengl.GLSurfaceView
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
-import java.nio.FloatBuffer
+import com.airhockey.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var glSurfaceView: GLSurfaceView? = null
+    private lateinit var binding: ActivityMainBinding
+    private var airHockeyRenderer: AirHockeyRenderer? = null
     private var rendererSet = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        glSurfaceView = GLSurfaceView(this)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initOpenGl()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (rendererSet) {
+            binding.glSurfaceView.onPause()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (rendererSet) {
+            binding.glSurfaceView.onResume()
+        }
+    }
+
+    private fun initOpenGl() {
         val activityManager = getSystemService(ACTIVITY_SERVICE) as? ActivityManager
         val configurationInfo = activityManager?.deviceConfigurationInfo
 
@@ -30,27 +47,12 @@ class MainActivity : AppCompatActivity() {
                 || (Build.MODEL.contains("Android SDK built for x86"))
         } ?: false
         if (supportEs2) {
-            glSurfaceView?.setEGLContextClientVersion(2)
-            glSurfaceView?.setRenderer(AirHockeyRenderer(this))
+            airHockeyRenderer = AirHockeyRenderer(this)
+            binding.glSurfaceView.setEGLContextClientVersion(2)
+            binding.glSurfaceView.setRenderer(airHockeyRenderer)
             rendererSet = true
         } else {
             Toast.makeText(this, "Device does not supports OpenGL ES 2.0\n$deviceGlEsVersion", Toast.LENGTH_LONG).show()
-            return
-        }
-        setContentView(glSurfaceView)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if (rendererSet) {
-            glSurfaceView?.onPause()
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if (rendererSet) {
-            glSurfaceView?.onResume()
         }
     }
 }
